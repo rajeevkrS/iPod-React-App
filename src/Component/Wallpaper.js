d84f40123ccd0ba4f87dfb412175cde6d40f66da
Rajeev <ronaldocrcr5@gmail.com>
2023-11-12 18:14:24 +0530

    setted up Wallpaper Items using map() method inside Wallpaper JS

--- src/Component/Wallpaper.js
+++ src/Component/Wallpaper.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React from "react";
 
-export default class Wallpaper extends React.Component{
+export default class Wallpaper extends React.Component {
+  render() {
+    const { active } = this.props;
 
-    render(){
-        return(
-            <>
-            
-            </>
-        )
-    }
+    return (
+      <div className="wallpaper">
+        <h2>Select Wallpaper</h2>
+
+        <ul>
+          {["Wallpaper 1", "Wallpaper 2", "Wallpaper 3"].map(
+            (element, index) => {
+              return active === index ? (
+                <li key={index} className="active theme-li">
+                  &nbsp; {element}
+                </li>
+              ) : (
+                <li key={index} className="theme-li">
+                  &nbsp; {element}
+                </li>
+              );
+            }
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
