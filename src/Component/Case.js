dc8e7bc00ca1b269d455cf206d1899a6e7b49803
Rajeev <ronaldocrcr5@gmail.com>
2023-11-12 15:31:47 +0530

    Passed all the state from parent appJs to child caseJs as a props and distributed all the props to display & wheel component.

--- src/Component/Case.js
+++ src/Component/Case.js
@@ -2,17 +2,51 @@ import React from "react";
 import Display from "./Display";
 import Wheel from "./Wheel";
 
+// This component is the outer case of iPod it does nothing special just renders display and wheel component
 export default class Case extends React.Component {
   constructor() {
     super();
   }
 
   render() {
+    const {active, updateActiveMenu, currentMenu, changeMenuForward, changeMenuBackward, menuItems, musicItems, togglePlayPause, songItems, playing, songIndex, theme, audio, songUrl, songImgUrl, seekSongForward, seekSongReverse, wheelColor, wallpaper, wallpaperItems, noty, setNoty, notifyText} = this.props;
+
     return (
-      <>
-        <Display />
-        <Wheel />
-      </>
+      <div className="case-container">
+        <div style={{backgroundColor: theme}} className="case">
+          <Display 
+            songIndex={songIndex}
+            playing={playing}
+            active={active}
+            musicItems={musicItems}
+            menuItems={menuItems}
+            currentMenu={currentMenu}
+            songItems={songItems}
+            audio={audio}
+            songUrl={songImgUrl}
+            songImgUrl={songImgUrl}
+            wallpaper={wallpaper}
+            wallpaperItems={wallpaperItems}
+            noty={noty}
+            setNoty={setNoty}
+            notifyText={notifyText}
+          />
+
+          <Wheel 
+            wheelColor={wheelColor}
+            theme={theme}
+            active={active}
+            menuItems={menuItems}
+            currentMenu={currentMenu}
+            changeMenuForward={changeMenuForward}
+            changeMenuBackward={changeMenuBackward}
+            updateActiveMenu={updateActiveMenu}
+            togglePlayPause={togglePlayPause}
+            seekSongForward={seekSongForward}
+            seekSongReverse={seekSongReverse}
+          />
+        </div>
+      </div>
     );
   }
 }
