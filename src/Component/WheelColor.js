08ce80f52f7cc9862bfbd11e4e7b9df9bab80986
Rajeev <ronaldocrcr5@gmail.com>
2023-11-12 17:54:16 +0530

    setted up wheelcolor using map() method

--- src/Component/WheelColor.js
+++ src/Component/WheelColor.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React from "react";
+import '../css/theme.css';
 
-export default class wheelColor extends React.Component{
+export default class wheelColor extends React.Component {
+  render() {
+    const { active } = this.props;
 
-    render(){
-        return(
-            <>
-            
-            </>
-        )
-    }
+    return (
+      <div className="wheelColor">
+        <h2>Select Wheel Color</h2>
+
+        <ul>
+          {["Black", "White", "Brown"].map(
+            (element, index) => {
+              return active === index ? (
+                <li key={index} className="active theme-li">
+                  &nbsp; {element}
+                </li>
+              ) : (
+                <li key={index} className="theme-li">
+                  &nbsp; {element}
+                </li>
+              );
+            }
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
