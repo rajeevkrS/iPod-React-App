d66a0d675d45e43f69b8c2ea22ea03bbe6a17187
Rajeev <ronaldocrcr5@gmail.com>
2023-11-12 17:44:34 +0530

    Setted up theme items in array using map() method and some styling

--- src/Component/Themes.js
+++ src/Component/Themes.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React from "react";
+import '../css/theme.css';
 
-export default class Themes extends React.Component{
+export default class Themes extends React.Component {
+  render() {
+    const { active } = this.props;
 
-    render(){
-        return(
-            <>
-            
-            </>
-        )
-    }
+    return (
+      <div className="theme">
+        <h2>Theme Select</h2>
+
+        <ul>
+          {["Snow White", "Black", "USC Gold", "Space Gray", "Pearl"].map(
+            (element, index) => {
+              return active === index ? (
+                <li key={index} className="active theme-li">
+                  &nbsp; {element}
+                </li>
+              ) : (
+                <li key={index} className="theme-li">&nbsp; {element}</li>
+              );
+            }
+          )}
+        </ul>
+      </div>
+    );
+  }
 }
